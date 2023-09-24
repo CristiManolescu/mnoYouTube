65a16b0c244fb6d2ded0c590976486f3dd89f232
CristiManolescu <m_cristian_s96@yahoo.com>
2023-09-24 14:28:10 +0300

    OnFocus + OnBlur search button

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,7 @@ import { YOUTUBE_SEARCH_API } from "../utils/constants";
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     //Debounce -> if you write faster than 200ms, then the API Call is canceled.
@@ -51,23 +52,27 @@ const Head = () => {
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="border border-gray-400 py-2 px-5 rounded-r-full bg-gray-100">
             🔎
           </button>
         </div>
-        <div className="fixed bg-white py-2 px-2 w-auto rounded-lg shadow-lg border border-gray-200">
-          <ul>
-            {suggestions.map((suggestion) => (
-              <li
-                key={suggestion}
-                className="py-2 px-3 shadow-sm hover:bg-gray-100"
-              >
-                🔎 {suggestion}
-              </li>
-            ))}
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-auto rounded-lg shadow-lg border border-gray-200">
+            <ul>
+              {suggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  className="py-2 px-3 shadow-sm hover:bg-gray-100"
+                >
+                  🔎 {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
